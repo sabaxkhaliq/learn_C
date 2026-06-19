42a36d873b617711345cc815a8a79e284de9bc6e
sabaxkhaliq <sabaxkhaliq@gmail.com>
2026-06-19 17:24:31 +0500

    Input_Output

---
+++ input_output.c
@@ -0,0 +1,12 @@
+//following example show the number as input
+#include<stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    int num;
+    printf("Enter any Number :: ");
+    scanf("%d", &num);
+    printf("The Number you Entered is %d\n", num);
+    
+    return 0;
+}
