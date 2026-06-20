2efe36e70c2155297043dbadbd9db4107894e10e
sabaxkhaliq <sabaxkhaliq@gmail.com>
2026-06-20 12:26:25 +0500

    Basics code

---
+++ check_num.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+// Input a number and check if it is greater than 50.
+
+int main()
+{
+    int a;
+
+    printf("Enter a Number: ");
+    scanf("%d", &a);
+
+    if (a > 50)
+    {
+        printf("A is greater than 50");
+    }
+    else
+    {
+        printf("A is less than or equal to 50");
+    }
+
+   
+}
---
+++ getch_function.c
@@ -0,0 +1,13 @@
+// program using getch function 
+#include<stdio.h>
+#include<conio.h>
+
+int main(int argc, char const *argv[])
+{
+
+
+    printf("Enter any key for terminate program :: ");
+    getch();
+
+    return 0;
+}
