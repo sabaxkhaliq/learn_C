1b33aa30ea1796f59e3c0f0c0e4467f4cecc70f4
sabaxkhaliq <sabaxkhaliq@gmail.com>
2026-06-25 11:26:17 +0500

    Basic Info

---
+++ basic_info.c
@@ -0,0 +1,11 @@
+// following program shows the height and age
+#include<stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    float height = 5.2;
+    int age = 16;
+
+    printf("My age is %d\nMy height is %f ", age, height);
+    return 0;
+}
