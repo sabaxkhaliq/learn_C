017ec8ff8178c049784f8ecb3ba5a769e6fbb034
sabaxkhaliq <sabaxkhaliq@gmail.com>
2026-07-01 17:43:05 +0500

    Area Of Square

---
+++ area_square.c
@@ -0,0 +1,15 @@
+// Area of square
+#include<stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    int length, square;
+    printf("Enter Length of Square :: ");
+    scanf("%d", &length);
+
+    square = length * 4;
+    printf("Area Of Square is :: %d", square);
+
+
+    return 0;
+}
